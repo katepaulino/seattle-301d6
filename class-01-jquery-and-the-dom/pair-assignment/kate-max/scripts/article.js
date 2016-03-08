93393f3bfc468256fbb57d8fecc9e3491ca02f36
Kate Lucas <katherinelucas@gmail.com>
2016-03-08 11:44:32 -0800

    edits

--- class-01-jquery-and-the-dom/pair-assignment/kate-max/scripts/article.js
+++ class-01-jquery-and-the-dom/pair-assignment/kate-max/scripts/article.js
@@ -16,21 +16,15 @@ Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
 
   $newArticle.attr('data-category', this.category);
-
-  // TODO: Use jQuery to fill in the template with properties
-  // from this particular Article instance. We need to fill in:
-  // the author name and url, the article title and body, and the
-  // publication date.
-
-  // Include the publication date as a 'title' attribute to show on hover:
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
-
-  // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-
   $newArticle.append('<hr>');
-
-  // TODO: This cloned article is no longer a template, so we should remove that class...
+  $newArticle.removeClass('template');
 
   return $newArticle;
 }
