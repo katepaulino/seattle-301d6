7d7a9a99955de7af69f16e4f91d20c0b0347f6ac
mevans72 <michaleevans72@gmail.com>
2016-03-22 09:25:46 -0700

    Making one minor change to articleController.js based on class code review.

--- class-11-spa-routing/pair-assignment/class11pp/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/class11pp/scripts/articleController.js
@@ -5,10 +5,10 @@
   // DONE: Complete this function below that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   Article.createTable();
-  Article.fetchAll(articleView.initIndexPage);
 
   // DONE: Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
+    Article.fetchAll(articleView.initIndexPage);
     $('.main-nav').on('click', '.tab', function(e) {
       $('.tab-content').hide();
       $('#articles').show();
