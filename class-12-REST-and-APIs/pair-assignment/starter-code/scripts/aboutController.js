5995b4e6a2b7c44c18d744ed8084c519632d6d83
Justin Lange <well1912@gmail.com>
2016-03-22 12:19:34 -0700

    Implemented aboutControler.index

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/aboutController.js
@@ -1,12 +1,19 @@
 (function(module) {
   var aboutController = {};
 
-  aboutController.index = function() {
+  aboutController.index = function(view) {
     $('#about').show().siblings().hide();
 
-    // TODO: Call a function to load (AKA 'request') our repo data.
+    // DONE: Call a function to load (AKA 'request') our repo data.
     // Pass in a view function as a callback, so our repos will render after the data is loaded.
-
+    $.ajax({
+      url: 'https://api.guthub.com/users/katepaulino/repos' +
+        '?per_page=5&sort=updated',
+      headers: {'Authorization': 'token ' + githubToken}
+      success: function (data) {
+        view(data);
+      }
+    });
   };
 
   module.aboutController = aboutController;
