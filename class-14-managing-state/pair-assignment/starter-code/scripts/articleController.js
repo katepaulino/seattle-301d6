3999d36384c0e444663f3dd02d3c4fb1035ba9c3
Mars <marshallmiller92@gmail.com>
2016-03-24 12:56:27 -0700

    added two more comments, load methods

--- class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
+++ class-14-managing-state/pair-assignment/starter-code/scripts/articleController.js
@@ -9,23 +9,24 @@
 
   // COMMENT: What does this method do?  What is it's execution path?
   //
-  articlesController.loadById = function(ctx, next) {
-    var articleData = function(article) {
-      ctx.articles = article;
-      next();
+  articlesController.loadById = function(ctx, next) { // loadById method on the articlesController object, assigning to the fucntion with the parameters ctx object and next callback
+    var articleData = function(article) { // declare the articleData var assigning to function with the article callback,
+      ctx.articles = article; // reassign the articles property to the context object to article
+      next(); // call the next callback, in the execution path this function will fire when page.js calls for the url /article/:id
     };
 
-    Article.findWhere('id', ctx.params.id, articleData);
+    Article.findWhere('id', ctx.params.id, articleData); // once the callback returns the context id, the findwhere method will execute as a callback to the webDB.execute to select all from articles where field = id
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
-  articlesController.loadByAuthor = function(ctx, next) {
-    var authorData = function(articlesByAuthor) {
-      ctx.articles = articlesByAuthor;
-      next();
+
+  articlesController.loadByAuthor = function(ctx, next) //loadByAuthormethod on articlesController object assigned to function with ctx and next params
+    var authorData = function(articlesByAuthor) { //delcared authorData set to function(articlesByAuthor)
+      ctx.articles = articlesByAuthor; //articles property of ctx object set to articlesByAuthor
+      next(); //callback in the path this function will fire when page.js calls for the url author/:authorName
     };
 
-    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData);
+    Article.findWhere('author', ctx.params.authorName.replace('+', ' '), authorData); //call back returns the context id, findWhere method that we defined will execute as a callback to the webDB to select all from articles where field = id
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
